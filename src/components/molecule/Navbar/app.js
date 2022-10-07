fd29285d58b89d4e8a0e6c75b54f62271dfb1db6
ovara1mark <markovara1@gmail.com>
2022-10-08 00:01:18 +0100

    update

--- src/components/molecule/Navbar/app.js
+++ src/components/molecule/Navbar/app.js
@@ -15,6 +15,10 @@ export const Nav = styled.div`
   @media screen and (max-width: 1230px) {
     padding: 2rem 0.5rem 2rem 0.5rem;
   }
+
+  @media screen and (max-width: 1230px) {
+    padding: 2rem 1rem 2rem 1rem;
+  }
 `;
 
 export const NavLeft = styled.div`
@@ -29,14 +33,53 @@ export const NavRight = styled.div`
   width: 60%;
 `;
 
-export const NavList = styled.div`
+export const NavList = styled.ul`
   display: flex;
   justify-content: space-between;
   padding-top: 7px;
+
+  @media screen and (max-width: 1000px) {
+    display: none;
+  }
 `;
 
-export const List = styled.div`
+export const ListChild = styled.ul`
+  padding: 1rem;
+  list-style: none;
+  background-color: #fffaf5;
+  box-shadow: 0 10px 30px 0rgba (45, 45, 45, 0.2);
+  position: absolute;
+  top: calc(100% + 0.25rem);
+  left: 0;
+  z-index: 9999999;
+  height: 10vh;
+  width: 10vw;
+  visibility: hidden;
+  transition: opacity 150ms ease-in-out, transform 150ms ease-in-out;
+`;
+
+export const List = styled.li`
   list-style-type: none;
   font-family: "Poppins", sans-serif;
   font-size: 13px;
+  position: relative;
+  padding: 0rem 1rem 0.5rem 1rem;
+`;
+
+export const NavListTwo = styled.div`
+  display: none;
+
+  @media screen and (max-width: 1000px) {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+
+export const ListDrop = styled.li`
+  list-style-type: none;
+  padding: 0.5rem 0rem;
+  color: black;
+  &:hover {
+    color: #ed5a45;
+  }
 `;
--- src/components/molecule/Navbar/index.jsx
+++ src/components/molecule/Navbar/index.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../assets/logowhite.png";
-import { Logo, Nav, NavLeft, NavRight, NavList, List } from "./app";
+import {
+  Logo,
+  Nav,
+  NavLeft,
+  NavRight,
+  NavList,
+  List,
+  NavListTwo,
+  ListChild,
+  ListDrop,
+} from "./app";
 import { BsCaretDownFill } from "react-icons/bs";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { CustomLink } from "../../atom";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState(false);
   return (
     <>
       <Nav>
@@ -24,7 +36,7 @@ export const Navbar = () => {
               </CustomLink>
             </List>
             <List>
-              <CustomLink to="/classes" className="link link-icon">
+              <CustomLink to="classes" className="link link-icon">
                 CLASSES
                 <BsCaretDownFill className="icon" />
               </CustomLink>
@@ -32,6 +44,10 @@ export const Navbar = () => {
             <List className="link link-icon">
               OTHERS
               <BsCaretDownFill className="icon" />
+              <ListChild className="list-child">
+                <ListDrop>Classes</ListDrop>
+                <ListDrop>Classes Detail</ListDrop>
+              </ListChild>
             </List>
             <List>
               <CustomLink to="/contact" className="link">
@@ -39,8 +55,49 @@ export const Navbar = () => {
               </CustomLink>
             </List>
           </NavList>
+          <NavListTwo>
+            <HiOutlineMenuAlt3
+              onClick={() => setMenu(!menu)}
+              className="hi-menu"
+            />
+          </NavListTwo>
         </NavRight>
       </Nav>
+      <div className={menu ? "show" : "hide"}>
+        <div className={menu ? "drop-menu-child" : "drop-menu--"}>
+          <NavList>
+            <List>
+              <CustomLink to="/" className="link">
+                HOME
+              </CustomLink>
+            </List>
+            <List>
+              <CustomLink to="about" className="link">
+                ABOUT
+              </CustomLink>
+            </List>
+            <List>
+              <CustomLink to="classes" className="link link-icon">
+                CLASSES
+                <BsCaretDownFill className="icon" />
+              </CustomLink>
+            </List>
+            <List className="link link-icon">
+              OTHERS
+              <BsCaretDownFill className="icon" />
+              <ListChild>
+                <ListDrop>Classes</ListDrop>
+                <ListDrop>Classes Detail</ListDrop>
+              </ListChild>
+            </List>
+            <List>
+              <CustomLink to="/contact" className="link">
+                CONTACT
+              </CustomLink>
+            </List>
+          </NavList>
+        </div>
+      </div>
     </>
   );
 };
